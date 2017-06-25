65f96b4efc96ba04bf6b44c97733565bd5e48e48
DESKTOP-LSSU7L6\marek <maruw4k@gmail.com>
2017-06-25 22:08:22 +0200

    Karuzela w js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('.owl-carousel').owlCarousel({
+    items: 1,
+    navigation: true,
+    pagination: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+ 
+  
+});
+
+});
